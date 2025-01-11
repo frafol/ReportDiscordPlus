83c4997b7e953362760d14c359c3d50366714f1f
Franc1es <ffrferrara@gmail.com>
2025-01-11 11:17:01 +0100

    6.5

--- src/main/java/bungee/me/francies/ReportDiscordPlus/utility/PlayerJoinListenerReports.java
+++ src/main/java/bungee/me/francies/ReportDiscordPlus/utility/PlayerJoinListenerReports.java
@@ -1,6 +1,7 @@
 package bungee.me.francies.ReportDiscordPlus.utility;
 
 import bungee.me.francies.ReportDiscordPlus.ReportDiscordPlus;
+import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PostLoginEvent;
@@ -35,7 +36,7 @@ public class PlayerJoinListenerReports implements Listener {
             if (openReports > 0) {
                 String messageTemplate = plugin.getConfig().getString("messages.openReportsMessage");
                 String message = messageTemplate.replace("{amount}", String.valueOf(openReports));
-                player.sendMessage(new TextComponent(message));
+                player.sendMessage(new TextComponent(ChatColor.translateAlternateColorCodes('&', message)));
             }
         }
     }
